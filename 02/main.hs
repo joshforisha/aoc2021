4a9d633380bc2957973afbf53ac29df3d3ac2966
Josh Forisha <josh@forisha.com>
2021-12-02 09:32:12 -0600

    Cleanup day 2

--- 02/main.hs
+++ 02/main.hs
@@ -7,26 +7,10 @@ main :: IO ()
 main = do
     course <- readFile "./02/course.txt"
     let directions = lines course
-    putStrLn $ "Position product: " ++ show (positionProduct directions)
-    putStrLn $ "Heading product: " ++ show (headingProduct directions)
-
-pair :: [String] -> (String, Int)
-pair [d, x] = (d, read x :: Int)
-
-step :: (String, Int) -> (Int, Int)
-step ("down", x) = (0, x)
-step ("forward", x) = (x, 0)
-step ("up", x) = (0, -x)
-step (_, _) = (0, 0)
-
-positionProduct :: [String] -> Int
-positionProduct directions =
-  let
-    (hpos, depth) = directions
-        & map (step . pair . words)
-        & foldr1 (\(h, d) (hx, dx) -> (h + hx, d + dx))
-  in
-    hpos * depth
+    let (hpos1, depth1) = position directions
+    putStrLn $ "Position product: " ++ show (hpos1 * depth1)
+    let (hpos2, depth2, _) = heading directions
+    putStrLn $ "Heading product: " ++ show (hpos2 * depth2)
 
 adjust :: (Int, Int, Int) -> (String, Int) -> (Int, Int, Int)
 adjust (h, d, a) ("down", x) = (h, d, a + x)
@@ -34,9 +18,17 @@ adjust (h, d, a) ("up", x) = (h, d, a - x)
 adjust (h, d, a) ("forward", x) = (h + x, d + (x * a), a)
 adjust y _ = y
 
-headingProduct :: [String] -> Int
-headingProduct directions =
-  let
-    (hpos, depth, _) = directions & map (pair . words) & foldl adjust (0, 0, 0)
-  in
-    hpos * depth
+heading :: [String] -> (Int, Int, Int)
+heading = foldl adjust (0, 0, 0) . map (pair . words)
+
+pair :: [String] -> (String, Int)
+pair [d, x] = (d, read x :: Int)
+
+position :: [String] -> (Int, Int)
+position = foldr1 (\(h, d) (hx, dx) -> (h + hx, d + dx)) . map (step . pair . words)
+
+step :: (String, Int) -> (Int, Int)
+step ("down", x) = (0, x)
+step ("forward", x) = (x, 0)
+step ("up", x) = (0, -x)
+step (_, _) = (0, 0)
