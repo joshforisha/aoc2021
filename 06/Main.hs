ed7ad4c489232f631e9ed7abb7b385254ef51108
Josh Forisha <josh@forisha.com>
2021-12-06 10:34:47 -0600

    Minor refactors to reduce repetition

--- 06/Main.hs
+++ 06/Main.hs
@@ -7,10 +7,10 @@ main :: IO ()
 main = do
     fishData <- readFile "./06/data.txt"
     let fishStr = map (\x -> read x :: Int) (items fishData)
-    let fish = foldl (\m k -> insertWith (+) k 1 m) empty fishStr
-    let simulate n = sum . elems . (!! n) . iterate update
-    putStrLn $ " 80 days: " ++ show (simulate 80 fish)
-    putStrLn $ "256 days: " ++ show (simulate 256 fish)
+    let fish = iterate update $ foldr (\k m -> insertWith (+) k 1 m) empty fishStr
+    let simulateDays n = sum . elems . (!! n) $ fish
+    putStrLn $ " 80 days: " ++ show (simulateDays 80)
+    putStrLn $ "256 days: " ++ show (simulateDays 256)
 
 items :: String -> [String]
 items s = l : case s' of
@@ -19,9 +19,9 @@ items s = l : case s' of
   where (l, s') = break (== ',') s
 
 update :: IntMap Int -> IntMap Int
-update = foldl up empty . assocs
+update = foldr up empty . assocs
   where
-    up = (\m (k, x) ->
+    up = (\(k, x) m ->
         if k == 0
-            then insertWith (+) 8 x . insertWith (+) 6 x $ m
+            then insertWith (+) 8 x (insertWith (+) 6 x m)
             else insertWith (+) (k - 1) x m)
