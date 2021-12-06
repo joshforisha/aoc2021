e3e14a37679af9cf9d21ea9d6bc7dfaa2e06c2bb
Josh Forisha <josh@forisha.com>
2021-12-06 10:23:17 -0600

    Refactor day 6 with IntMap and inline definitions

--- 06/Main.hs
+++ 06/Main.hs
@@ -1,20 +1,17 @@
 module Main (main) where
 
-import Data.Function ((&))
-import Data.Map (Map)
+import Data.IntMap (IntMap, assocs, elems, empty, insertWith)
 import System.IO
-import qualified Data.Map as M
-
-type Fish = Map Int Int
+import qualified Data.IntMap as M
 
 main :: IO ()
 main = do
     fishData <- readFile "./06/data.txt"
     let fishStr = map (\x -> read x :: Int) (items fishData)
-    let fish = foldl (\m k -> M.insertWith (+) k 1 m) M.empty fishStr
-    let simulate n = (!!n) . iterate update
-    putStrLn $ " 80 days: " ++ (show . population . simulate 80) fish
-    putStrLn $ "256 days: " ++ (show . population . simulate 256) fish
+    let fish = foldl (\m k -> insertWith (+) k 1 m) empty fishStr
+    let simulate n = sum . elems . (!! n) . iterate update
+    putStrLn $ " 80 days: " ++ show (simulate 80 fish)
+    putStrLn $ "256 days: " ++ show (simulate 256 fish)
 
 items :: String -> [String]
 items s = l : case s' of
@@ -22,13 +19,10 @@ items s = l : case s' of
     (_:s'') -> items s''
   where (l, s') = break (== ',') s
 
-population :: Fish -> Int
-population = sum . M.elems
-
-update :: Fish -> Fish
-update = foldl up M.empty . M.assocs
+update :: IntMap Int -> IntMap Int
+update = foldl up empty . assocs
   where
     up = (\m (k, x) ->
         if k == 0
-            then (M.insert 8 x . M.insert 6 x) m
-            else M.insertWith (+) (k - 1) x m)
+            then insertWith (+) 8 x . insertWith (+) 6 x $ m
+            else insertWith (+) (k - 1) x m)
